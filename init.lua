a2c0bf8bf527ab308732bab1e9c4b88f64bbdaf5
Dikshant Wavhule <dikshantw11@gmail.com>
2025-02-22 08:53:12 +0000

    treesitter setup

--- init.lua
+++ init.lua
@@ -1,10 +1,11 @@
 require("config.lazy")
 
+vim.opt.clipboard = "unnamedplus"
 
 vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking text',
-	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = 'Highlight when yanking text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
