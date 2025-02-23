a192b894ca1929db320b4c584b315497779ead08
Dikshant Wavhule <dikshantw11@gmail.com>
2025-02-23 14:53:38 +0000

    telescope setup

---
+++ lua/config/plugins/telescope.lua
@@ -0,0 +1,28 @@
+vim.g.mapleader = " "
+return {
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
+    config = function()
+      require('telescope').setup {
+        picker = {},
+        extensions = {
+          fzf = {}
+        }
+      }
+      require('telescope').load_extension('fzf')
+      vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
+      vim.keymap.set("n", "<leader>fg", require('telescope.builtin').live_grep, {})
+      vim.keymap.set("n", "<space>en", function()
+        require('telescope.builtin').find_files {
+          cwd = vim.fn.stdpath("config")
+        }
+      end)
+      require "config.telescope.multigrep".setup()
+    end
+  }
+}
