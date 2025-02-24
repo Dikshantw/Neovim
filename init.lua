01c2ba21a80c885484126b23d61b7cf25ccf59f6
Dikshant Wavhule <dikshantw11@gmail.com>
2025-02-24 21:31:23 +0000

    remapping

--- init.lua
+++ init.lua
@@ -1,11 +1,12 @@
 require("config.lazy")
 
 vim.opt.clipboard = "unnamedplus"
-
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking text',
-	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = 'Highlight when yanking text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
