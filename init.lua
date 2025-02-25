5b2fa60af6e76c81e6bb14028cee36021845ab63
Dikshant Wavhule <dikshantw11@gmail.com>
2025-02-25 18:24:20 +0000

    remapping

--- init.lua
+++ init.lua
@@ -1,12 +1,24 @@
-require("config.lazy")
+vim.g.mapleader = " "
 
 vim.opt.clipboard = "unnamedplus"
-vim.g.mapleader = " "
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.breakindent = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 10
+--set highlight on search, but clear on pressing <Esc>
+--vim.opt.hlsearch = true
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
 })
+require("config.lazy")
--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,27 +1,28 @@
 return {
   {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
     config = function()
-      require('telescope').setup {
+      require("telescope").setup({
         picker = {},
         extensions = {
-          fzf = {}
-        }
-      }
-      require('telescope').load_extension('fzf')
-      vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
-      vim.keymap.set("n", "<leader>fg", require('telescope.builtin').live_grep, {})
+          fzf = {},
+        },
+      })
+      require("telescope").load_extension("fzf")
+      vim.keymap.set("n", "<space>fd", require("telescope.builtin").find_files)
+      vim.keymap.set("n", "<space>vh", require("telescope.builtin").help_tags)
+      vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, {})
       vim.keymap.set("n", "<space>en", function()
-        require('telescope.builtin').find_files {
-          cwd = vim.fn.stdpath("config")
-        }
+        require("telescope.builtin").find_files({
+          cwd = vim.fn.stdpath("config"),
+        })
       end)
-      require "config.telescope.multigrep".setup()
-    end
-  }
+      require("config.telescope.multigrep").setup()
+    end,
+  },
 }
