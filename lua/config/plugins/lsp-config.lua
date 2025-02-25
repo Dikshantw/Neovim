99fa0b40241440212620200c8eda036e3fe1571f
Dikshant Wavhule <dikshantw11@gmail.com>
2025-02-25 10:14:39 +0000

    added autocompletion

--- lua/config/plugins/lsp-config.lua
+++ lua/config/plugins/lsp-config.lua
@@ -9,24 +9,29 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls" }
+        ensure_installed = { "lua_ls", "ts_ls" },
       })
-    end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.ts_ls.setup({})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.ts_ls.setup({ capabilities = capabilities })
 
-      vim.api.nvim_create_autocmd('LspAttach', {
+      vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
           local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if not client then return end
+          if not client then
+            return
+          end
 
-          if client.supports_method('textDocument/formatting') then
-            vim.api.nvim_create_autocmd('BufWritePre', {
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_create_autocmd("BufWritePre", {
               buffer = args.buf,
               callback = function()
                 vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
@@ -34,10 +39,11 @@ return {
             })
           end
         end,
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {}),
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {}),
-        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {}),
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {}),
+        vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {}),
+        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {}),
       })
-    end
-  }
+    end,
+  },
 }
